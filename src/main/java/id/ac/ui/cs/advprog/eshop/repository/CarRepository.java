3c60fe6c96453387dd3fa32390d8a8a32146d961
naylafn <naylafarah.31@gmail.com>
2025-02-25 05:02:24 +0700

    feat: car repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 public class CarRepository {
     static int id = 0;
     private List<Car> carData = new ArrayList<>();
+
     public Car create(Car car) {
         if(car.getCarId() == null){
             UUID uuid = UUID.randomUUID();
@@ -23,4 +24,28 @@ public class CarRepository {
     public Iterator<Car> findAll() {
         return carData.iterator();
     }
+
+    public Car findById(String carId) {
+        for(Car car : carData){
+            if(car.getCarId().equals(carId)){
+                return car;
+            }
+        }
+        return null;
+    }
+
+    public Car update(String carId, Car updatedCar) {
+        for(int i = 0; i < carData.size(); i++) {
+            Car car = carData.get(i);
+            if(car.getCarId().equals(carId)){
+                car.setCarName(updatedCar.getCarName());
+                car.setCarColor(updatedCar.getCarColor());
+                car.setCarQuantity(updatedCar.getCarQuantity());
+                return car;
+            }
+        }
+        return null;
+    }
+
+    public void delete(String carId) { carData.removeIf(car -> car.getCarId().equals(carId));}
 }
