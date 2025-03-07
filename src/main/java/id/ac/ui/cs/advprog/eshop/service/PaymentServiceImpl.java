367b2ca94a3efba06fb1d97c83db158be3aa82d7
naylafn <naylafarah.31@gmail.com>
2025-03-07 17:04:30 +0700

    [GREEN] Implement PaymentServiceImpl #2

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import id.ac.ui.cs.advprog.eshop.enums.*;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
@@ -20,18 +21,35 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentDetails) {
-        return null;
+        Payment payment = new Payment("7ff4472c-9f47-4612-9a7c-e4ea361df456", order, method, paymentDetails);
+        paymentRepository.save(payment);
+        return payment;
     }
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        Order order = orderRepository.findById(payment.getId());
+        if (order != null) {
+            if (status.equals("SUCCESS")) {
+                order.setStatus(OrderStatus.SUCCESS.getValue());
+            } else if (status.equals("REJECTED")) {
+                order.setStatus(OrderStatus.FAILED.getValue());
+            } else {
+                throw new IllegalArgumentException();
+            }
+            orderRepository.save(order);
+            payment.setStatus(status);
+            paymentRepository.save(payment);
+        } else {
+            throw new IllegalArgumentException();
+        }
+        return payment;
     }
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.getAllPayments();
     }
 }
